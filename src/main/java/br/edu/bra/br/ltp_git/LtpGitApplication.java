a10cce4fdcc6925a9f1b94e75963860ef1d196f4
unknown <karenazevedopaiva@gmail.com>
2024-09-05 16:15:00 -0300

    Criação do projeto

---
+++ src/main/java/br/edu/bra/br/ltp_git/LtpGitApplication.java
@@ -0,0 +1,13 @@
+package br.edu.bra.br.ltp_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtpGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtpGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/bra/br/ltp_git/LtpGitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.bra.br.ltp_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtpGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
